49d9aa67dce3163bb28e2f31cf2a9a75f27e453c
Praveen Kumar Pandey <95954302+techpraveen@users.noreply.github.com>
2025-02-16 02:37:01 +0530

    Update vite.config.js
    some changes are  made

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './',  // Ensures correct path resolution for static hosting
+  build: {
+    outDir: 'dist', // Output directory
+    assetsDir: 'assets', // Store static assets separately
+    emptyOutDir: true, // Clear the folder before each build
+  },
+});
